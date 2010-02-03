f44cc7f4ef6987db450ce7f92741ae668034448c
dyoo <dyoo@kfisler-ra1.(none)>
2010-02-03 18:58:44 -0500

    ripping out some content

--- contrib/scheme/js/tokenizescheme.js
+++ contrib/scheme/js/tokenizescheme.js
@@ -56,13 +56,10 @@ var tokenizeScheme = (function() {
     // Wrapper around schemeToken that helps maintain parser state (whether
     // we are inside of a multi-line comment and whether the next token
     // could be a regular expression).
-    function schemeTokenState(inside, regexp) {
+    function schemeTokenState() {
 	return function(source, setState) {
-	    var newInside = inside;
-	    var type = schemeToken(inside, regexp, source, function(c) {newInside = c;});
-	    var newRegexp = type.type == "operator" || type.type == "keyword c" || type.type.match(/^[\[{}\(,;:]$/);
-	    if (newRegexp != regexp || newInside != inside)
-		setState(schemeTokenState(newInside, newRegexp));
+	    var type = schemeToken(source, 
+				   function(c) {newInside = c;});
 	    return type;
 	};
     }
@@ -71,7 +68,7 @@ var tokenizeScheme = (function() {
     // tokenize.js (through schemeTokenState). Advances the source stream
     // over a token, and returns an object containing the type and style
     // of that token.
-    function schemeToken(inside, regexp, source, setInside) {
+    function schemeToken(source, setInside) {
 	function readHexNumber(){
 	    source.next(); // skip the 'x'
 	    source.nextWhileMatches(isHexDigit);
@@ -101,32 +98,6 @@ var tokenizeScheme = (function() {
 	    return known ? {type: known.type, style: known.style, content: word} :
 	    {type: "variable", style: "scheme-symbol", content: word};
 	}
-	function readRegexp() {
-	    nextUntilUnescaped(source, "/");
-	    source.nextWhileMatches(/[gi]/);
-	    return {type: "regexp", style: "scheme-string"};
-	}
-	// Mutli-line comments are tricky. We want to return the newlines
-	// embedded in them as regular newline tokens, and then continue
-	// returning a comment token for every line of the comment. So
-	// some state has to be saved (inside) to indicate whether we are
-	// inside a /* */ sequence.
-	function readMultilineComment(start){
-	    var newInside = "/*";
-	    var maybeEnd = (start == "*");
-	    while (true) {
-		if (source.endOfLine())
-		    break;
-		var next = source.next();
-		if (next == "/" && maybeEnd){
-		    newInside = null;
-		    break;
-		}
-		maybeEnd = (next == "*");
-	    }
-	    setInside(newInside);
-	    return {type: "comment", style: "scheme-comment"};
-	}
 	function readOperator() {
 	    source.nextWhileMatches(isOperatorChar);
 	    return {type: "operator", style: "scheme-operator"};
@@ -140,17 +111,12 @@ var tokenizeScheme = (function() {
 
 	// Fetch the next token. Dispatches on first character in the
 	// stream, or first two characters when the first is a slash.
-	if (inside == "\"")
-	    return readString(inside);
 	var ch = source.next();
-	if (inside == "/*") {
-	    return readMultilineComment(ch);
-	}
-	else if (ch == "\"") {
+	if (ch == "\"") {
 	    return readString(ch);
 	}
 	// with punctuation, the type of the token is the symbol itself
-	else if (/[\[\]{}\(\),;\:\.\']/.test(ch)) {
+	else if (/[\[\]{}\(\),\:\.\']/.test(ch)) {
 	    return {type: ch, style: "scheme-punctuation"};
 	}
 	else if (ch == "0" && (source.equals("x") || source.equals("X"))) {
@@ -160,18 +126,9 @@ var tokenizeScheme = (function() {
 	    return readNumber();
 	}
 	else if (ch == ";") {
+	    console.log("comment");
 	    nextUntilUnescaped(source, null);
 	    return { type: "comment", style: "scheme-comment"};
-// 	    if (source.equals("*"))
-// 	    { source.next(); return readMultilineComment(ch); }
-// 	    else if (source.equals("/")) { 
-// 		nextUntilUnescaped(source, null);
-// 		return {type: "comment", style: "js-comment"};
-// 	    }
-// 	    else if (regexp)
-// 		return readRegexp();
-// 	    else
-// 		return readOperator();
 	}
 	else if (isOperatorChar.test(ch)) {
 	    return readOperator();
@@ -183,7 +140,7 @@ var tokenizeScheme = (function() {
 
     // The external interface to the tokenizer.
     return function(source, startState) {
-	return tokenizer(source, startState || schemeTokenState(false, true));
+	return tokenizer(source, startState || schemeTokenState());
     };
 })();
 
