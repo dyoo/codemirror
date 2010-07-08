cd6916ead0978662615124954108d7c263e15332
Danny Yoo <dyoo@cs.wpi.edu>
2010-07-08 18:11:56 -0400

    chasing whitespace tokenization issues

--- contrib/scheme/js/tokenizescheme.js
+++ contrib/scheme/js/tokenizescheme.js
@@ -14,6 +14,12 @@
 
 
 var tokenizeScheme = (function() {
+    var isWhiteSpace = function(ch) {
+	// The messy regexp is because IE's regexp matcher is of the
+	// opinion that non-breaking spaces are no whitespace.
+	return ch != "\n" && /^[\s\u00a0]*$/.test(ch);
+    };
+
 
     // scanUntilUnescaped: string-stream char -> boolean
     // Advances the stream until the given character (not preceded by a
@@ -46,6 +52,8 @@ var tokenizeScheme = (function() {
     // Some helper regexps
     var isHexDigit = /[0-9A-Fa-f]/;
     
+
+    var whitespaceChar = new RegExp("[\\s\\u00a0]");
     
     var isDelimiterChar = 
 	new RegExp("[\\s\\\(\\\)\\\[\\\]\\\{\\\}\\\"\\\,\\\'\\\`\\\;]");
@@ -163,11 +171,26 @@ var tokenizeScheme = (function() {
 	};
 
 
+	var readWhitespace = function() {
+	    source.nextWhile(isWhiteSpace);
+	    var content = source.get();
+	    return { type: 'whitespace', style:'whitespace', content: content };
+	};
+
+	var readNewline = function() {
+	    var content = source.get();
+	    return { type:'whitespace', style:'whitespace', content: content };
+	};
+
 
 	// Fetch the next token. Dispatches on first character in the
 	// stream, or first two characters when the first is a slash.
 	var ch = source.next();
-	if (ch === "#") {
+	if (ch === '\n') {
+	    return readNewline();
+	} else if (whitespaceChar.test(ch)) {
+	    return readWhitespace();
+	} else if (ch === "#") {
 	    return readPound();
 	} else if (ch ===';') {
 	    return readLineComment();
@@ -188,23 +211,16 @@ var tokenizeScheme = (function() {
 
     var makeTokenizer = function(source, state) {
 	// Newlines are always a separate token.
-	var isWhiteSpace = function(ch) {
-	    // The messy regexp is because IE's regexp matcher is of the
-	    // opinion that non-breaking spaces are no whitespace.
-	    return ch != "\n" && /^[\s\u00a0]*$/.test(ch);
-	}
 
 	var tokenizer = {
 	    state: state,
 
 	    take: function(type) {
-		if (typeof(type) == "string")
-		    type = {style: type, type: type};
+ 		if (typeof(type) == "string")
+ 		    type = {style: type, type: type};
 
-		type.content = (type.content || "") + source.get();
-		if (!/\n$/.test(type.content))
-		    source.nextWhile(isWhiteSpace);
-		type.value = type.content + source.get();
+ 		type.content = (type.content || "") + source.get();
+		type.value = type.content;
 		return type;
 	    },
 
@@ -212,23 +228,13 @@ var tokenizeScheme = (function() {
 		if (!source.more()) throw StopIteration;
 
 		var type;
-		if (source.equals("\n")) {
-		    source.next();
-		    var result = this.take("whitespace");
-		    return result;
-		}
-		
-		if (source.applies(isWhiteSpace)) {
-		    type = "whitespace";
-		} else {
-		    while (!type) {
-			type = tokenizer.state(source, function(s) {
-			    tokenizer.state = s;
-			});
-		    }
+		while (!type) {
+		    type = tokenizer.state(source, function(s) {
+			tokenizer.state = s;
+		    });
 		}
 		var result = this.take(type);
-		return result;
+		return result;		    
 	    }
 	};
 	return tokenizer;
